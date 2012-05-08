96f2eacfd375e4fbea246fb6bd355564baf76598
Jason Bentley <jbxfade@fullsail.edu>
2012-05-08 16:36:37 -0700

    First variables assigned

--- Bentley_Jason_assignment2.js
+++ Bentley_Jason_assignment2.js
@@ -1,1 +1,14 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
+
+// Assignment: Project 2
+// Author: Jason Bentley
+// Created for: SDI Online 1205
+// First assignment: Project 2: Teaching at an Audio School
+
+var audio = "Recording Program",
+	film = "Film Program",
+	numStudents = "18"
+	instructors = "Alan Mason and Jason Bentley"
+	labTopics = ["Introduction", "Monitor Path Connection", "Effects and Inserts", "Input Path Connection", "Quiz" ]
+
+
